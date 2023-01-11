620c92c8fb9ee7e344a5f0197c7ec13258958a8e
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2023-01-11 23:37:46 +0100

    11-01-2023

---
+++ LastPageChecker/MyTypes/Strings.fs
@@ -0,0 +1,15 @@
+﻿module Strings
+
+open System
+
+let private myOption (s: string) (fn: string) = 
+    s 
+    |> Option.ofObj 
+    |> function 
+        | Some value -> value
+        | None       -> fn //some action
+
+type StringNonN (s: string) = member this.StringNonN = myOption s      
+
+let (|StringNonN|) s = myOption s
+
